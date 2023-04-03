0946b685788f1571c568d66438fa10924a3145aa
Julien Widmer <ju.widmer@pm.me>
2023-04-03 10:08:29 -0400

    Retrieve and display missions from API

--- 101320111-lab-test2-comp3133/src/app/missionlist/missionlist.component.ts
+++ 101320111-lab-test2-comp3133/src/app/missionlist/missionlist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-missionlist',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./missionlist.component.css']
 })
 export class MissionlistComponent {
-  title = "SpaceX Mission Launch"
+  title = "SpaceX Mission Launch";
+  missions: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  // Called once Angular has initialized all
+  // data-bound properties
+  ngOnInit() {
+    this.http.get<any[]>("https://api.spacexdata.com/v3/launches").subscribe({
+      next: (missions) => this.missions = missions,
+      error: (error) => console.error(error),
+      complete: () => console.info("Missions retrieval complete.")
+    });
+  }
 }
