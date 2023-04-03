561e2d4d226a6a556296b1eb2859ec4c269ecf9a
Julien Widmer <ju.widmer@pm.me>
2023-04-03 09:09:38 -0400

    Create missionlist component

--- 101320111-lab-test2-comp3133/src/app/app.module.ts
+++ 101320111-lab-test2-comp3133/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionlistComponent
   ],
   imports: [
     BrowserModule
